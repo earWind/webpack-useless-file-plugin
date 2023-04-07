6d881e4278add731ae9a4c58b3277f42eb85b701
linjuncai <linjuncai>
2023-04-07 16:19:14 +0800

    create

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # webpack-useless-file-plugin
+
+```js
+ plugins: [
+    new UselessFilePlugin({
+        root: path.resolve(__dirname, './src'), // 项目目录
+        out: './fileList.json', // 输出文件列表
+        clean: false, // 是否删除文件
+        exclude: /node_modules/ // 排除文件列表
+    })
+ ]
+```
---
+++ UselessFilePlugin.js
@@ -0,0 +1,101 @@
+const fs = require('fs')
+const glob = require('glob')
+const path = require('path')
+const shelljs = require('shelljs')
+
+// 查找没有用到的文件
+class UselessFilePlugin {
+  constructor(options) {
+    this.opts = options
+  }
+  apply(compiler) {
+    compiler.hooks.emit.tap('useless-file', (compilation) => {
+      this.findUnusedFiles(compilation)
+    })
+  }
+
+  /**
+   * 获取依赖的文件
+   */
+  getDependFiles(compilation) {
+    return new Promise((resolve, reject) => {
+      const dependedFiles = [...compilation.fileDependencies].reduce(
+        (acc, usedFilepath) => {
+          if (!~usedFilepath.indexOf('node_modules')) {
+            acc.push(usedFilepath)
+          }
+          return acc
+        },
+        []
+      )
+      resolve(dependedFiles)
+    })
+  }
+
+  /**
+   * 获取项目目录所有的文件
+   */
+  getAllFiles(pattern) {
+    return new Promise((resolve, reject) => {
+      glob(
+        pattern,
+        {
+          nodir: true
+        },
+        (err, files) => {
+          if (err) {
+            throw err
+          }
+          const out = files.map((item) => path.resolve(item))
+          resolve(out)
+        }
+      )
+    })
+  }
+
+  dealExclude(path, unusedList) {
+    const file = fs.readFileSync(path, 'utf-8')
+    const files = JSON.parse(file) || []
+    const result = unusedList.filter((unused) => {
+      return !files.some((item) => ~unused.indexOf(item))
+    })
+    return result
+  }
+
+  async findUnusedFiles(compilation) {
+    const {
+      root = './src',
+      clean = false,
+      output = './unused-files.json',
+      exclude = false
+    } = this.opts
+    const pattern = root + '/**/*'
+    try {
+      const allChunks = await this.getDependFiles(compilation)
+      const allFiles = await this.getAllFiles(pattern)
+      let unUsed = allFiles.filter((item) => {
+        const isvj = item.includes('.vue') || item.includes('.js')
+        return isvj && !~allChunks.indexOf(item)
+      })
+      if (exclude && typeof exclude === 'string') {
+        unUsed = this.dealExclude(exclude, unUsed)
+      }
+      if (typeof output === 'string') {
+        fs.writeFileSync(output, JSON.stringify(unUsed, null, 4))
+      } else if (typeof output === 'function') {
+        output(unUsed)
+      }
+      if (clean) {
+        unUsed.forEach((file) => {
+          shelljs.rm(file)
+          console.log(`remove file: ${file}`)
+        })
+      }
+      return unUsed
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+module.exports = UselessFilePlugin
